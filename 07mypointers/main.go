74c6ce08b7eb7fa641a512834a5e01df0e4b0529
Sydney Mainga <user@users-MacBook-Pro.local>
2023-01-18 16:18:02 +0300

    pointers

---
+++ 07mypointers/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("welcome to pointers")
+	// var ptr *int
+	// fmt.Println("value of pointer is: ", ptr)
+	myNumber := 23
+
+	var ptr = &myNumber
+
+	fmt.Println("value of actual pointer is :", ptr)
+	fmt.Println("value of actual pointer is :", *ptr)
+
+	*ptr = *ptr + 2
+
+	fmt.Println("new value is :", myNumber)
+
+}
